84fec7fe89f3d6b59043510513a55042629c2a72
blacknoob20 <crguerrero@crguerrero>
2021-01-29 22:57:26 -0500

    Changes: Clase Base de datos y querybuilder Migraciones SQL: terminado. Crear las tablas con sentencias SQL. Archivo: database/migrations/2021_01_29_212342_create_usuarios_table.php

--- database/migrations/2021_01_29_212342_create_usuarios_table.php
+++ database/migrations/2021_01_29_212342_create_usuarios_table.php
@@ -13,17 +13,25 @@ class CreateUsuariosTable extends Migration
      */
     public function up()
     {
-        Schema::create('usuarios', function (Blueprint $table) {
-            $table->increments('id');
-            $table->string('nombre,255');
-            $table->string('email,255');
-            $table->string('clave,255');
-            $table->integer('edad');
-            // Columna agregada despues de la tabla
-            $table->integer('sueldo');
-            // Crea por defecto 2 columnas tipo fecha (created_at y updated_at)
-            $table->timestamps();
-        });
+        // Schema::create('usuarios', function (Blueprint $table) {
+        //     $table->increments('id');
+        //     $table->string('nombre,255');
+        //     $table->string('email,255');
+        //     $table->string('clave,255');
+        //     $table->integer('edad');
+        //     // Columna agregada despues de la tabla
+        //     $table->integer('sueldo');
+        //     // Crea por defecto 2 columnas tipo fecha (created_at y updated_at)
+        //     $table->timestamps();
+        // });
+        $sql = "CREATE TABLE usuarios(
+            id int auto_increment not null,
+            nombre varchar(255),
+            email varchar(255),
+            clave varchar(255),
+            primary key(id)
+        );";
+        DB::statement($sql);
     }
 
     /**
